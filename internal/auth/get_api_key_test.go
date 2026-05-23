186a8c0c04fad06d3eb8bf1ca6308bf6aaec321d
delroscol98 <collindelrosario98@gmail.com>
2026-05-24 00:02:24 +1000

    fixed unit tests

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -14,7 +14,7 @@ func TestGetAPIKey(t *testing.T) {
 	tests := []test{
 		{input: http.Header{"Authorization": []string{"ApiKey 1234567890abc"}}, want: "1234567890abc"},
 		{input: http.Header{"Authorization": []string{"ApiKey"}}, want: ""},
-		{input: http.Header{"Authorization": []string{"NotApiKey"}}, want: "hello"},
+		{input: http.Header{"Authorization": []string{"NotApiKey"}}, want: ""},
 		{input: http.Header{"Authorization": []string{""}}, want: ""},
 	}
 
